d21777360660de602cdaf1aedc6703a56e64d3a3
ankurjhaaa <kumarankur7258@gmail.com>
2026-05-02 12:58:48 +0530

    add transition component for improved UI animations and state management

---
+++ public/build/assets/InputLabel-V6GfHQsO.js
@@ -0,0 +1,1 @@
+import{j as m}from"./app-BZg8EPBl.js";function o({value:t,className:e="",children:r,...s}){return m.jsx("label",{...s,className:"block text-sm font-medium text-gray-700 "+e,children:t||r})}export{o as I};
